b93a728deb00abdf96ada095a903a786b46b5310
Jacob Frantz <jacobrossfrantz@gmail.com>
2023-06-26 19:21:29 -0400

    minimal repro in index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: "64629af2-1ebe-4cac-90dc-b8cdc856ff6e",
+        initialAuthenticationMode: 'connect-only',
+      }}
+    >
+      <DynamicWagmiConnector>
+        <Component {...pageProps} />
+      </DynamicWagmiConnector>
+    </DynamicContextProvider>
+  );
 }
