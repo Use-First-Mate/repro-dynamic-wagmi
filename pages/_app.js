906c4e53a814a033b7df3b6fb972be0c49ea0915
Jacob Frantz <jacobrossfrantz@gmail.com>
2023-06-30 10:37:31 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,21 @@ import "@/styles/globals.css";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 
-const staging = "5764ca4e-b339-4d3a-a485-6e274b3edf04"
-const prod = "64629af2-1ebe-4cac-90dc-b8cdc856ff6e"
+const stagingWithWC2 = "5764ca4e-b339-4d3a-a485-6e274b3edf04"
+const prodWithoutWC2 = "64629af2-1ebe-4cac-90dc-b8cdc856ff6e"
 export default function App({ Component, pageProps }) {
   return (
     <DynamicContextProvider
-      settings={{
-        environmentId: prod,
+/*       settings={{
+        environmentId: prodWithoutWC2,
         initialAuthenticationMode: 'connect-only',
         walletConnectV1Bridge: 'https://derelay.rabby.io/',
-      }}
-/*    // turn on v2 and note that network switching doesnt work
-      settings={{
-        environmentId: staging,
-        initialAuthenticationMode: 'connect-and-sign',
       }} */
+   // turn on v2 and note that network switching doesnt work
+      settings={{
+        environmentId: stagingWithWC2,
+        initialAuthenticationMode: 'connect-and-sign',
+      }}
     >
       <DynamicWagmiConnector>
         <Component {...pageProps} />
